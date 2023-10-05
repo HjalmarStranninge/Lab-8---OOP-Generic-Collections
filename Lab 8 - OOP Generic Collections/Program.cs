f71f12625f943a715792d8d40ef009f05157849e
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-10-05 11:41:28 +0200

    Add project files.

---
+++ Lab 8 - OOP Generic Collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_8___OOP_Generic_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

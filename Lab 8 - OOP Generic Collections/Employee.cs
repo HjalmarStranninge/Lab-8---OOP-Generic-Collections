6a44791f4f4b654feb83ac82afe3193ea014e2eb
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2023-10-05 13:58:09 +0200

    Quick cleanup

--- Lab 8 - OOP Generic Collections/Employee.cs
+++ Lab 8 - OOP Generic Collections/Employee.cs
@@ -9,13 +9,17 @@ namespace Lab_8___OOP_Generic_Collections
     // Creating the class 'Employee'.
     internal class Employee
     {
+
         // The properties for Employee.
+
         public string ID { get; set; }
         public string Name { get; set; }  
         public string Gender { get; set; }
         public int Salary { get; set; }  
 
+
         // Constructor for the class
+
         public Employee(string id, string name, string gender, int salary)
         {
             ID = id;
@@ -25,10 +29,10 @@ namespace Lab_8___OOP_Generic_Collections
         }
 
         // Overrides the ToString() method to print the properties of the class.
+
         public override string ToString()
         {
             return $"ID: {ID} - Name: {Name} - Gender: {Gender} - Salary: {Salary} $/year";
         }
-
     }
 }
